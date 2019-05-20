0cde95d4bbb58b2b3435ee6a75dfb0defc3c11c2
Tuskemx <ma.edwards@live.com>
2019-05-20 16:44:47 +0100

    onto date bit

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
+
 const tasks = [...document.getElementsByTagName('li')]
-
-console.log(tasks);
-
 const addTask = document.getElementById("addtask");
+const Dates = [...document.getElementsByClassName("date")]
 
-addTask.addEventListener('click', ()=> {
-let text = document.getElementById("add").value;
-let node = document.createElement("li")
-node.innerText = text;
-document.getElementById("theList").appendChild(node)
- 
+
+// console.log(Dates);
+
+// const date = new Date();
+// const year = date.getFullYear();
+// const month = date.getMonth();
+// const day = date.getDay();
+
+// const currDate = [year, month, day];
+
+// console.log(currDate);
+let secondsDate = new Date();
+
+
+let current_datetime = new Date()
+let formatted_date = current_datetime.getDate() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getFullYear()
+console.log(formatted_date)
+
+
+
+
+addTask.addEventListener('click', () => {
+    let text = document.getElementById("add").value;
+    let node = document.createElement("li")
+    node.innerText = text;
+    document.getElementById("theList").appendChild(node)
+    node.addEventListener('click', function changeState() {
+        if (node.className === 'lineThrough') {
+            node.setAttribute('class', 'deline')
+        }
+        else {
+            node.setAttribute('class', 'lineThrough')
+        }
+
+    })
 })
-
-
-
 
 
 tasks.forEach(item => item.addEventListener('click', function changeState() {
